7860040b45fee5e51118fe55cea51db1343ba064
Garin90 <alex.garrido.baldrich@gmail.com>
2023-05-24 11:08:44 +0200

    provisional shop model CRUD

--- api/configs/routes.config.js
+++ api/configs/routes.config.js
@@ -7,8 +7,8 @@ router.post('/shops', shopsController.create)
 router.patch('/shops/:id', shopsController.update) //I'm not sure if it's patch that I need for shops updating
 router.delete('/shops/:id', shopsController.delete)
 
-router.get('/products', productsControllers.list)
-router.get('/products/:id', productsController.detail)
-router.post('/products', productsController.create)
-router.patch('/products/:id', productsController.update)
-router.delete('/products/:id', productsController.delete)
+// router.get('/products', productsControllers.list)
+// router.get('/products/:id', productsController.detail)
+// router.post('/products', productsController.create)
+// router.patch('/products/:id', productsController.update)
+// router.delete('/products/:id', productsController.delete)
--- api/controllers/shop.constrollers.js
+++ api/controllers/shop.constrollers.js
@@ -6,8 +6,25 @@ module.exports.list = (req, res, next) => {
     .catch(next)
 }
 
-module.exports.detail = (req, res, next) = {
+module.exports.detail = (req, res, next) => {
   Shop.findById(req.params.id)
     .then((shop) => res.json(shop))
     .catch(next)
+}
+
+module.exports.create = (req, res, next) => {
+  Shop.create(req.body)
+    .then((body) => res.json(body))
+    .catch(next)
+}
+
+module.exports.update = (req, res, next) => {
+  Shop.findByIdAndUpdate(req.params.id, req.body)
+    .then((shopUpdated) => res.json(shopUpdated))
+    .catch(next)
+}
+
+module.exports.delete = (req, res, next) => {
+  Shop.findByIdAndDelete(req.params.id)
+    .then((shopDeleted) => console.log(`Shop deleted: ${shopDeleted}`))
 }
